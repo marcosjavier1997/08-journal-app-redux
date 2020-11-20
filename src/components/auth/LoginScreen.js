c775ea17eebf9bf6e60a95f688444d85a04edaf5
Marcos Gomez <marcos_go2015@hotmail.com>
2020-11-20 05:29:53 -0300

    6. Primer dispatch de una acción a nuestro Store

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,15 +1,33 @@
 import React from 'react'
-
+import {useDispatch} from 'react-redux'
 import { Link } from 'react-router-dom'
+import { useForm } from '../../hooks/useForm'
+import { login } from './../../actions/auth';
 
 export const LoginScreen = () => {
+
+    const dispatch = useDispatch()
+
+
+    const [formValues, handleInputChange] = useForm({
+        email: 'marcos_go2015@hotmail.com',
+        password: '123456'
+    })
+
+    const {email,password} = formValues;
+
+    const handleLogin = (e) =>{
+        e.preventDefault();
+        dispatch(login(12345,'MARCOS'))
+    }
+
     return (
         <>
             <h3 className="auth__title">Login</h3>
 
-            <form>
-                <input type = "text" placeholder= "Email" name= "email" autoComplete="off "className="auth__input"/>
-                <input type = "password" placeholder= "Password" name= "password" className="auth__input"/>
+            <form onSubmit={handleLogin}>
+                <input type = "text" placeholder= "Email" name= "email" autoComplete="off "className="auth__input" value = {email} onChange={handleInputChange}/>
+                <input type = "password" placeholder= "Password" name= "password" className="auth__input" value={password} onChange={handleInputChange}/>
                 <button type="submit" className = "btn btn-primary btn-block" >Login</button>
 
                 <div className="auth__social-networks">
