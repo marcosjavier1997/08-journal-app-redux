94635a282ded5fdfd930218694d75298e9e2dd7d
Marcos Gomez <marcos_go2015@hotmail.com>
2020-11-20 18:53:16 -0300

    10. Formularo de registro de usuarios

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,17 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from './../../hooks/useForm';
 
 
 export const RegisterScreen = () => {
+
+    const [formValues, handleInputChange] = useForm({
+        name: 'Marcos',
+        email: 'marcos_go2015@hotmail.com',
+        password: '123',
+        password2: '123'
+    })
+    const {name,email,password, password2} = formValues;
+
+    const handleRegister = (e) =>{
+        e.preventDefault();
+        //console.log(name, email, password, password2);
+
+
+
+    }
+/* 
+    const isFormValid = () =>{
+
+    }
+ */
+
     return (
         <>
             <h3 className="auth__title">Register</h3>
 
-            <form>
-                <input type = "text" placeholder= "Name" name= "name" autoComplete="off "className="auth__input"/>
-                <input type = "text" placeholder= "Email" name= "email" autoComplete="off "className="auth__input"/>
-                <input type = "password" placeholder= "Password" name= "password" className="auth__input"/>
-                <input type = "password" placeholder= "Confirm password" name= "password2" className="auth__input"/>
+            <form onSubmit={handleRegister}>
+                <input type = "text" placeholder= "Name" name= "name" autoComplete="off "className="auth__input" value={name} onChange={handleInputChange}/>
+                <input type = "text" placeholder= "Email" name= "email" autoComplete="off "className="auth__input"value={email} onChange={handleInputChange}/>
+                <input type = "password" placeholder= "Password" name= "password" className="auth__input"value={password} onChange={handleInputChange}/>
+                <input type = "password" placeholder= "Confirm password" name= "password2" className="auth__input"value={password2} onChange={handleInputChange}/>
                 <button type="submit" className = "btn btn-primary btn-block mb-5" >Register</button>
 
                 
