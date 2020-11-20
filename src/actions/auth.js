ff4e675b9b7ccf102afb0a5476a033e341a7dba0
Marcos Gomez <marcos_go2015@hotmail.com>
2020-11-20 18:14:45 -0300

    8. Thunk Middleware - Acciones asíncronas

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,6 +1,18 @@
 
 import { types } from './../types/types';
 
+export const startLoginEmailPassword = (email,password) =>{
+    return(dispatch) => {
+
+        
+
+        setTimeout(() => {
+            dispatch(login(123,"JAVIER"))
+        }, 3500);
+
+    }
+}
+
 export const login = (uid, displayName) => 
     ({
         type: types.login,
