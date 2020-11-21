a864d0961d7c2aa4820a15c944527561ae8a5cf2
Marcos Gomez <marcos_go2015@hotmail.com>
2020-11-21 13:55:55 -0300

    19. Logout de Firebase

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import { JournalEntries } from './JournalEntries';
+import { useDispatch } from 'react-redux';
+import { startLogout } from './../../actions/auth';
 
 export const Sidebar = () => {
+
+    const dispatch = useDispatch();
+
+    const handleLogout = () =>{
+        dispatch(startLogout())
+    }
+
     return (
         <aside className="journal__sidebar">
             <div className="journal__sidebar-navbar">
@@ -10,7 +19,7 @@ export const Sidebar = () => {
                     <span> Marcos!</span>
                 </h3>
 
-                <button className="btn">
+                <button className="btn" onClick={handleLogout}>
                     Logout
                 </button>
 
